f9aad6e642fa8ca6f83b067d5dc996172c0c332b
Ben Eccles <ben.g.eccles@icloud.com>
2019-09-16 14:36:44 -0600

    completed step 3

--- src/components/Name/Name.js
+++ src/components/Name/Name.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import store, { UPDATE_NAME, UPDATE_CATEGORY} from "../../store";
+
 import "./Name.css";
 
 class Name extends Component {
@@ -10,6 +12,7 @@ class Name extends Component {
       category: ''
     };
   }
+
   handleNameChange(nameVal) {
     this.setState({
       name: nameVal
@@ -23,6 +26,14 @@ class Name extends Component {
   }
   saveChanges() {
     // Send data to Redux state
+    store.dispatch({
+      type: UPDATE_NAME,
+      payload: this.state.name
+    });
+    store.dispatch({
+      type: UPDATE_CATEGORY,
+      payload: this.state.category
+    });
   }
   render() {
     return (
--- src/store.js
+++ src/store.js
@@ -1,1 +1,31 @@
+import { createStore } from 'redux'
 
+//Initial STATE
+const initialState = {
+    name: "",
+    category: ""
+}
+
+// ACTION CONSTANTS
+export const UPDATE_NAME = 'UPDATE_NAME'
+export const UPDATE_CATEGORY = 'UPDATE_CATEGORY'
+
+// Reducer
+function reducer(state = initialState, action) {
+    const {type, payload } = action
+    switch(type) {
+        case UPDATE_NAME:
+            // ... operator will return a new object with the same data.
+            // "copy the rest of state in an immutable way.""
+            return {...state, name: payload}
+        case UPDATE_CATEGORY:
+            return {...state, category: payload}
+        default:
+            return state
+    }
+}
+
+export default createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
